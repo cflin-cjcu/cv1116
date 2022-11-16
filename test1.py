48b52d86ba123e0fc5e887235bf2930be95f29e6
cflin <cflin@mail.cjcu.edu.tw>
2022-11-16 10:42:58 +0800

    FPS

--- test1.py
+++ test1.py
@@ -1,16 +1,20 @@
 from FaceDetectionModule import FaceDetector
 import cv2
 import pafy
-url='https://www.youtube.com/watch?v=3kPH7kTphnE'
+import cvzone
+
+url='https://www.youtube.com/watch?v=Da41FWyAklE'
 videoPafy = pafy.new(url)
 best = videoPafy.getbest()
-
+fpsReader = cvzone.FPS()
 # cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture(best.url)
+cap.set(3, 1280)
+cap.set(4, 720)
 detector = FaceDetector()
 while True:
     success, img = cap.read()
-    img = cv2.resize(img,(1024,768))
+    # img = cv2.resize(img,(1024,768))
     img, bboxs = detector.findFaces(img) ###########
     detector = FaceDetector(modelSelection=1,minDetectionCon=0.3)
     if bboxs:
@@ -19,6 +23,7 @@ while True:
         print(bboxs)
         cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
 
+    fps, img = fpsReader.update(img,pos=(50,80),color=(0,255,0),scale=5,thickness=5)
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
